b05a236bdb96e37ca7f8af5f6f5cf5d7517b4c78
chadmark <89610897+chadmark@users.noreply.github.com>
2026-03-23 15:03:14 -0700

    Create HEADER_TEMPLATE.ps1
    Header template for all scripts in this repository

---
+++ HEADER_TEMPLATE.ps1
@@ -0,0 +1,11 @@
+.SYNOPSIS    - one-liner summary
+.DESCRIPTION - fuller explanation
+.NOTES
+    Author      : Chad
+    Last Edit   : MM/DD/YYYY
+    GitHub      : https://github.com/chadmark/MSP-Scripts/blob/main/<ScriptName>.ps1
+    Environment : where/how it runs
+    Requires    : dependencies/rights needed
+    Version     : x.x
+.LINK
+    https://github.com/chadmark/MSP-Scripts
